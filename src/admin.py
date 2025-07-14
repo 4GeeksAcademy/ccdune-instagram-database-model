bb5d835a9c346c00969c4a3e2af95c0347c43559
Clay Beach <wcbeach30@gmail.com>
2025-07-14 19:48:00 +0000

    Added the models into the admin.py file

--- src/admin.py
+++ src/admin.py
@@ -1,47 +1,7 @@
 import os
 from flask_admin import Admin
-from models import db, User
+from models import db, User, Follower, Media, Post, Comment
 from flask_admin.contrib.sqla import ModelView
-
-
-class BookView(ModelView):
-    column_list = [
-        'id', 'title', 'author', 'num_pages',
-        'date_published', 'cover', 'is_awesome'
-    ]
-
-
-class AuthorView(ModelView):
-    column_list = [
-        'id', 'name', 'dob', 'gender', 'books'
-    ]
-
-
-class GenreView(ModelView):
-    column_list = [
-        'id', 'name', 'books'
-    ]
-
-
-class UserView(ModelView):
-    column_list = [
-        'id', 'username', 'email', '_password'
-    ]
-
-
-def setup_admin(app):
-    app.secret_key = os.environ.get('FLASK_APP_KEY', 'sample key')
-    app.config['FLASK_ADMIN_SWATCH'] = 'slate'
-    admin = Admin(app, name='4Geeks Admin', template_mode='bootstrap4')
-
-    # Add your models here, for example this is how we add a the User model to the admin
-    admin.add_view(ModelView(User, db.session))
-    admin.add_view(AuthorView(Author, db.session))
-    admin.add_view(BookView(Book, db.session))
-    admin.add_view(GenreView(Genre, db.session))
-
-    # You can duplicate that line to add mew models
-    # admin.add_view(ModelView(YourModelName, db.session))
 
 
 class FollowerView(ModelView):
@@ -52,7 +12,22 @@ class FollowerView(ModelView):
 
 class UserView(ModelView):
     column_list = [
+        'ID', 'username', 'firstname', 'lastname', 'email'
+    ]
 
+class MediaView(ModelView):
+    column_list = [
+        'ID', 'type', 'url', 'post_id'
+    ]
+
+class PostView(ModelView):
+    column_list = [
+        'ID', 'user_ID'
+    ]
+
+class CommentView(ModelView):
+    column_list = [
+        'ID', 'comment_text', 'author_id', 'post_id'
     ]
 
 
@@ -62,8 +37,11 @@ def setup_admin(app):
     admin = Admin(app, name='4Geeks Admin', template_mode='bootstrap3')
 
     # Add your models here, for example this is how we add a the User model to the admin
-    admin.add_view(FollowerView(User, db.session))
-    admin.add_view(ModelView(Media, db.session))
+    admin.add_view(FollowerView(Follower, db.session))
+    admin.add_view(UserView(User, db.session))
+    admin.add_view(MediaView(Media, db.session))
+    admin.add_view(PostView(Post, db.session))
+    admin.add_view(CommentView(Comment, db.session))
 
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
